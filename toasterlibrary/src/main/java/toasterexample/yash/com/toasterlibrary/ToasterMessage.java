0bee0a7a51de5f21913a97ea9ae3893221f3b905
Yash Madaan <yash@apporio.com>
2019-02-17 21:25:10 +0530

    Initial Commit

---
+++ toasterlibrary/src/main/java/toasterexample/yash/com/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,12 @@
+package toasterexample.yash.com.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
